c0c5ad9eb0ad13807638603df97d51afbf3e0f12
Kyle Lane <kylelane@v1020-wn-0-224.campus-dynamic.uwaterloo.ca>
2018-05-09 12:21:04 -0400

    Added some libraries for svgs and charts. some changes to scores tab.

--- android/app/src/main/java/com/deepstatsapp/MainApplication.java
+++ android/app/src/main/java/com/deepstatsapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.deepstatsapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import io.realm.react.RealmReactPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new RealmReactPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
--- src/Containers/commonComponents/GameScore.js
+++ src/Containers/commonComponents/GameScore.js
@@ -14,10 +14,10 @@ import {
     Image,
 } from 'react-native'
 import { List, ListItem, SearchBar, Avatar } from 'react-native-elements'
-import { colors, teamColors, windowSize, appFonts } from '../../styles/commonStyles'
+import { colors, teamColors, windowSize, appFonts, fontSize } from '../../styles/commonStyles'
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 import moment from 'moment'
-
+import { BarChart, Labels } from 'react-native-svg-charts'
 
 import SVGImage from 'react-native-svg-image'
 
@@ -36,7 +36,49 @@ export default class GameScore extends React.Component {
         // }
     }
 
-    renderMiddleView(boxscore, profile, broadcasters) {
+    renderMiddleView(item) {
+        const {boxscore, profile, broadcasters, homeTeam, awayTeam} = item;
+        let leader = 'none';
+        if(boxscore.homeScore > boxscore.awayScore) {
+            leader = 'home';
+        } else if (boxscore.homeScore < boxscore.awayScore) {
+            leader = 'away';
+        }
+        // const fillHome = teamColors[homeTeam.profile.abbr].primary;
+        const fillHome = colors.mainAccent;
+        // const fillAway = teamColors[awayTeam.profile.abbr].primary;
+        const fillAway = colors.mainAccent
+
+        let dataHome = [];
+        let dataAway = [];
+
+        dataAway.push(boxscore.homeScore);
+        dataAway.push(boxscore.awayScore);
+        dataAway.push(boxscore.homeScore);
+
+        dataHome.push(boxscore.homeScore * -1);
+        dataHome.push(boxscore.awayScore * -1);
+        dataHome.push(boxscore.homeScore * -1);
+
+        console.log('homeScore', dataHome);
+        // const CUT_OFF = 200;
+        // const Labels = ({  x, y, bandwidth, data }) => (
+        //     dataHome.map((value, index) => (
+        //         <Text
+        //             key={ index }
+        //             x={ value > CUT_OFF ? x(0) + 10 : x(value) - 10 }
+        //             y={ y(index) + (bandwidth / 2) }
+        //             fontSize={ 3 }
+        //             fill={ value > CUT_OFF ? 'white' : 'black' }
+        //             alignmentBaseline={ 'middle' }
+        //         >
+        //             {value}
+        //         </Text>
+        //     ))
+        // );
+
+
+        const data   = [ 50, 10, 40, 95, -4, -24, null, 85, undefined, 0, 35, 53, -53, 24, 50, -20, -80 ];
         let name = 'TBD';
         if(broadcasters[0] && broadcasters[0].name) {
             name = broadcasters[0].name;
@@ -55,14 +97,37 @@ export default class GameScore extends React.Component {
             );
         }
         return(
-            <View>
+            <View style={styles.columnContainer}>
                 <Text>
                     <Text style={styles.timeText}>{moment(date.toString()).format('h:mm a')}</Text>
                     <Text style={styles.timeText, styles.secondaryText}>  </Text>
                     <Text style={styles.timeText}>{name}</Text>
                 </Text>
-                <View>
-                    
+                <View style={styles.graphRow}>
+                    <Text style={[styles.scoresText, leader==='away' && styles.underlineScore]}>{boxscore.homeScore}</Text>
+                    <Text style={[styles.scoresText, styles.secondaryText]}>  -  </Text>
+                    <Text style={[styles.scoresText, leader==='home' && styles.underlineScore]}>{boxscore.awayScore}</Text>
+
+                    {/*<BarChart*/}
+                        {/*style={{ height: 40, width: windowSize.width * 0.2 }}*/}
+                        {/*data={ dataHome }*/}
+                        {/*svg={{ fill: fillHome }}*/}
+                        {/*// contentInset={{ top: 16, bottom: 16 }}*/}
+                        {/*horizontal={true}*/}
+                        {/*gridMin={0}*/}
+                    {/*>*/}
+                        {/*/!*<Labels />*!/*/}
+                    {/*</BarChart>*/}
+                    {/*<BarChart*/}
+                        {/*style={{ flex: 1, height: 40, width: windowSize.width * 0.2 }}*/}
+                        {/*data={ dataAway }*/}
+                        {/*svg={{ fill: fillAway }}*/}
+                        {/*// contentInset={{ top: 16, bottom: 16 }}*/}
+                        {/*horizontal={true}*/}
+                        {/*gridMin={0}*/}
+                    {/*>*/}
+                    {/*</BarChart>*/}
+
                 </View>
             </View>
         );
@@ -81,7 +146,7 @@ export default class GameScore extends React.Component {
                     {this.renderBottomText(homeTeam, boxscore)}
                 </View>
                 <View>
-                    {this.renderMiddleView(boxscore, profile, broadcasters)}
+                    {this.renderMiddleView(this.props.item)}
                 </View>
                 <View style={styles.columnContainer}>
                     <SVGImage
@@ -132,6 +197,23 @@ const styles = StyleSheet.create({
         color: colors.baseText
     },
     secondaryText: {
+        color: colors.secondaryText
+    },
+    graphRow: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexWrap: 'nowrap',
+        // width: 300,
+        backgroundColor: 'transparent',
+        paddingTop: 8
+        // height: 100
+    },
+    scoresText: {
+        ...appFonts.xlBold,
+        color: colors.mainTextColor
+    },
+    underlineScore: {
         color: colors.secondaryText
     }
 });
